8e9aa2a4671b8937d1add0b328c249c9eaf293c9
MilicTG <milic.tg@gmail.com>
2023-03-08 17:47:00 +0100

    Videos from 1-5

---
+++ lib/features/movie_flow/movie_flow.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:movie_recommendation_app/features/movie_flow/landing/landing_screen.dart';
+
+class MovieFlow extends StatefulWidget {
+  const MovieFlow({super.key});
+
+  @override
+  State<MovieFlow> createState() => _MovieFlowState();
+}
+
+class _MovieFlowState extends State<MovieFlow> {
+  final pageController = PageController();
+
+  void nextPage() {
+    pageController.nextPage(
+      duration: const Duration(milliseconds: 600),
+      curve: Curves.easeInOutCubic,
+    );
+  }
+
+  void previousPage() {
+    pageController.previousPage(
+      duration: const Duration(milliseconds: 600),
+      curve: Curves.easeInOutCubic,
+    );
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: pageController,
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+        LandingScreen(nextPage: nextPage, previousPage: previousPage),
+        Scaffold(
+          body: Container(color: Colors.green),
+        ),
+        Scaffold(
+          body: Container(color: Colors.blue),
+        ),
+        Scaffold(
+          body: Container(color: Colors.yellow),
+        ),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:movie_recommendation_app/theme/custom_theme.dart';
+
+import 'features/movie_flow/movie_flow.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +12,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'Movie Recommendation',
+      darkTheme: CustomTheme.darkTheme(context),
+      themeMode: ThemeMode.dark,
+      home: const MovieFlow(),
     );
   }
 }
